2fd313087d04894b1d776c71eda3d462b4b12af3
seunghun yang <yabby1997@gmail.com>
2021-03-13 20:10:41 +0900

    36. Let’s develop a complex Grid Layout system with four Toolbar Buttons

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,32 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Seunghun Yang on 2021/03/13.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    // MARK: - PROPERTIES
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+// MARK: - PREVIEW
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[1])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
