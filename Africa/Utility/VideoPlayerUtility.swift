8f25775824ce197919e4f2348e2c301e45afb2b1
seunghun yang <yabby1997@gmail.com>
2021-03-13 13:41:02 +0900

    31. Let’s create a Video Player with SwiftUI 2 framework - Part 2

---
+++ Africa/Utility/VideoPlayerUtility.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerUtility.swift
+//  Africa
+//
+//  Created by Seunghun Yang on 2021/03/13.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
