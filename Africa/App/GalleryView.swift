dbe4ba077a49af0b5a261f2408b77ab20b740fc6
seunghun yang <yabby1997@gmail.com>
2021-03-13 17:43:57 +0900

    35. Let’s create a photo gallery with Grid Layout using SwiftUI 2

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,11 +9,53 @@ import SwiftUI
 
 struct GalleryView: View {
     // MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridColumn: Double = 3.0
+    @State private var gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            Image(selectedAnimal)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 8))
+            
+            Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding(.horizontal)
+                .onChange(of: gridColumn, perform: { value in
+                    gridSwitch()
+                })
+            
+            VStack {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                } //:VGRID
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
